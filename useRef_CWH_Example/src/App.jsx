73ec3e3baa759092c7a714ef5341f8b6b6c77c40
jatinn21 <thisisjatinn@gmail.com>
2024-03-08 19:45:02 +0530

    Added One more Example pf useRef : to target any DOM Element. pass ref={refName} in the element

--- useRef_CWH_Example/src/App.jsx
+++ useRef_CWH_Example/src/App.jsx
@@ -5,22 +5,27 @@ import { useRef } from "react";
 
 function App() {
   let a = useRef(0);
+  let TargetButton = useRef();
   const [count, setCount] = useState(0);
 
   function clickedFun() {
-    // setCount(count + 1);  // uncommit this line to make the rerendered after every click
+    setCount(count + 1); // uncommit this line to make the rerendered after every click
     console.log("rerendered, Value Fetched : " + a.current++); // this will work : )
   }
 
   useEffect(() => {
     console.log(`rerendering happened....`);
+    TargetButton.current.style.backgroundColor =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
     //  change in useRef value will never make rerendering
   });
   //  Every time it will return 0 only because every time state changes it will make rerender
 
   return (
     <>
-      <button onClick={clickedFun}>Click me</button>
+      <button ref={TargetButton} onClick={clickedFun}>
+        Click me
+      </button>
 
       <br />
       <br />
