a9c9f321132d2c68cb36d23899e7ad3214c2e5d2
jatinn21 <thisisjatinn@gmail.com>
2024-03-13 10:39:56 +0530

    Done with uncontrolled Form data Flow

--- React_onboarding_form_Uncontrolled_Design_Practice/src/App.jsx
+++ React_onboarding_form_Uncontrolled_Design_Practice/src/App.jsx
@@ -1,39 +1,57 @@
 import React, { useState } from "react";
 import OnBoardingForm from "./OnBoardingForm";
 
-const Step1 = (onNextStep) => {
+const Step1 = ({ onNextStep, onPreviousStep }) => {
   return (
     <div>
       <h1>Step 1</h1>
       <br />
-      <button>Previous</button>
+      <button onClick={onPreviousStep}>Previous</button>
       <br />
       <br />
 
-      <button onClick={onNextStep}>Next</button>
+      <button
+        onClick={() => {
+          onNextStep({ name: "Jatin Sharma" });
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 };
-const Step2 = (onNextStep) => {
+const Step2 = ({ onNextStep, onPreviousStep }) => {
   return (
     <div>
       <h1>Step 2</h1>
       <br />
-      <button>Previous</button>
+      <button onClick={onPreviousStep}>Previous</button>
       <br />
       <br />
-      <button onClick={onNextStep}>Next</button>
+      <button
+        onClick={() => {
+          onNextStep({ age: 22 });
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 };
-const Step3 = (onNextStep) => {
+const Step3 = ({ onNextStep, onPreviousStep }) => {
   return (
     <div>
       <h1>Step 3</h1>
       <br />
-      <button>Previous</button>
+      <button onClick={onPreviousStep}>Previous</button>
       <br />
-      <button onClick={onNextStep}>Next</button>
+      <button
+        onClick={() => {
+          onNextStep({ role: "React developer" });
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 };
@@ -42,7 +60,9 @@ function App() {
   return (
     <>
       <h1>Hello</h1>
-      <OnBoardingForm>
+      <OnBoardingForm
+        onFinish={(data) => console.log("hi from onFinish from App : ", data)}
+      >
         <Step1 />
         <Step2 />
         <Step3 />
--- React_onboarding_form_Uncontrolled_Design_Practice/src/OnBoardingForm.jsx
+++ React_onboarding_form_Uncontrolled_Design_Practice/src/OnBoardingForm.jsx
@@ -1,11 +1,55 @@
 import React, { useState } from "react";
 
 function OnBoardingForm({ children, onFinish }) {
-  const [onBoardingComponent, SetOnBoardingComponentComponent] = useState({});
+  //  For inserting the data that we get from each Step like first Basic Details, Education Details then Terms and conditions agreement
+  const [onBoardingData, SetOnBoardingData] = useState({});
   const [currentComponentIndex, setCurrentComponentIndex] = useState(0);
 
-  const currentComponentDisplayed = React.Children.toArray(children);
-  return currentComponentDisplayed[currentComponentIndex];
+  const currentChildDisplayed =
+    React.Children.toArray(children)[currentComponentIndex];
+
+  // Function to be executed when previous button is pressed
+  const onPreviousStep = () => {
+    let prevIndex = currentComponentIndex - 1;
+    if (prevIndex < 0) {
+      alert("This is Step 1,\n Press Next to move forward");
+      return;
+    }
+    setCurrentComponentIndex(prevIndex);
+  };
+
+  // Function to be executed when next button is pressed
+  const onNextStep = (CurrentStepData) => {
+    let nextIndex = currentComponentIndex + 1;
+
+    // Basically  we are adding the next data which we get from current Step in our existing data
+    const updatedData = { ...onBoardingData, ...CurrentStepData };
+
+    console.log(updatedData);
+
+    //  Checking the boundary whether the next step is there or not
+    if (nextIndex < children.length) {
+      setCurrentComponentIndex(nextIndex);
+    } else {
+      alert("This is Last Step ,\n Press Previous to go to Back");
+
+      onFinish(updatedData);
+    }
+
+    // Updating our the old stored data into new onBoarding Data which includes current step data too : )
+    SetOnBoardingData(updatedData);
+  };
+
+  // Clone banaya or props usko pass kar diya
+  if (React.isValidElement(currentChildDisplayed)) {
+    return React.cloneElement(currentChildDisplayed, {
+      onNextStep,
+      onPreviousStep,
+    });
+  }
+
+  // return kara denge
+  return currentChildDisplayed;
 }
 
 export default OnBoardingForm;
